20b80479383ec7a470918435e2dcb126ee181216
Njogu-brian <briannjogu85@gmail.com>
2020-10-04 22:09:31 +0300

    add dw formula

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ function getAkanName (){
     let female = docuemnt.getEleemntById("female")
 
     docuemnt.getElementById("submit").onclik = function () {
-        let gender, bdtae;
+        let gender, DateOfBirth;
 
         bdate =date_input.nodeValue;
 
@@ -18,6 +18,19 @@ function getAkanName (){
                 <p class="text-sm">Kindly fill in the options to learn your Akan name</p>
             </div>
             ;
+        }
+
+        if (DateofBirth != '' gender !='' {
+
+            let d = new Date(DateofBirth);
+            let day = d.getDay();
+            let month = d.getMonth();
+            let year = d.getFullYear().toString();
+            let CC = year.substring(o, 2);
+
+            dw = ((((CC / 4) - 2) * (CC - 1)) + ((5 * year) / 4) + ((26 * (month + 1) / 10)) +
+                        day) % 7;
+            
         }
    
 }
