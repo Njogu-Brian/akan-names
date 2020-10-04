6de72124fd15f195e52a2539b7fdad055d357331
Njogu-brian <briannjogu85@gmail.com>
2020-10-04 19:49:54 +0300

    add variables and fuction for gender elemnts

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,23 @@
 function getAkanName (){
-    var days =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-    var maleAkanNames =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
-    var femaleAkanNames =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+    let date_input = document.getElementById("birthday")
+    let male = document.getElementById("male")
+    let female = docuemnt.getEleemntById("female")
+
+    docuemnt.getElementById("submit").onclik = function () {
+        let gender, bdtae;
+
+        bdate =date_input.nodeValue;
+
+        if (male.checked)   {
+            gender = "male";
+        }   else if (female.checked){
+            gender = "female";
+        }   else {
+            document.getElementById("akan-name").innerHTMML = 
+            <div cladd ="alert alert-danger" role="alert">
+                <p class="text-sm">Kindly fill in the options to learn your Akan name</p>
+            </div>
+            ;
+        }
+   
 }
